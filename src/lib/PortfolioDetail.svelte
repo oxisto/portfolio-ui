7f0c832a903305371a38b5bf3b26bacdaee1939a
Christian Banse <oxisto@aybaze.com>
2021-10-26 12:51:56 +0200

    More consitent variable naming

--- src/lib/PortfolioDetail.svelte
+++ src/lib/PortfolioDetail.svelte
@@ -5,13 +5,19 @@
     import { number } from "svelte-i18n";
 
     import type { Portfolio, PortfolioSnapshot } from "./portfolio";
-    import type { SecurityPosition, Security } from "./security";
+    import type { SecurityPosition } from "./security";
     import { file } from "./global";
 
     export let portfolio: Portfolio;
-    let entries: SecurityPosition[] = [];
+
+    let snapshot: PortfolioSnapshot;
     let totalMarketValue: number = 0;
     let totalProfit = 0;
+
+    let numberLocale = /*Intl.NumberFormat().resolvedOptions().locale*/ "de";
+
+    // TODO: fetch this from the portfolio API
+    let currency = "EUR";
 
     onMount(async () => {
         const apiUrl = `/api/v0/portfolios/${portfolio.uuid}/assets`;
@@ -23,20 +29,14 @@
         })
             .then((res) => res.json())
             .then((response: PortfolioSnapshot) => {
-                entries = response.positions;
+                snapshot = response;
 
-                totalMarketValue = calculateTotalMarketValue(entries);
-                totalProfit = calculateTotalProfit(entries);
+                totalMarketValue = calculateTotalMarketValue(
+                    snapshot.positions
+                );
+                totalProfit = calculateTotalProfit(snapshot.positions);
             });
     });
-
-    function profitOrLoss(position: SecurityPosition): string {
-        if (position.record.capitalGainsOnHoldings.amount < 0) {
-            return "loss";
-        } else {
-            return "profit";
-        }
-    }
 
     function calculateTotalMarketValue(positions: SecurityPosition[]): number {
         var total = 0;
@@ -59,78 +59,107 @@
     }
 </script>
 
-<div id="details">
-    <h5>{portfolio.name}</h5>
+{#if snapshot}
+    <div id="details">
+        <h5>{portfolio.name}</h5>
 
-    <div
-        class="container"
-        style="width: max-content; margin-left: 0; margin-right: 0; padding-left: 0"
-    >
-        <div class="row">
-            <div class="col-sm">
-                <div class="market-value-title">Total Market Value</div>
+        <div
+            class="container"
+            style="width: max-content; margin-left: 0; margin-right: 0; padding-left: 0"
+        >
+            <div class="row">
+                <div class="col-sm">
+                    <div class="market-value-title">Total Market Value</div>
 
-                <div class="market-value">
-                    {$number(totalMarketValue.toFixed(2), {
-                        locale: "en",
-                    })} €
+                    <div class="market-value">
+                        {$number(totalMarketValue, {
+                            locale: numberLocale,
+                            style: "currency",
+                            currency: "EUR",
+                        })}
+                    </div>
                 </div>
-            </div>
-            <div class="col-sm">
-                <div class="market-value-title">Total Profit / Loss</div>
+                <div class="col-sm">
+                    <div class="market-value-title">Total Profit / Loss</div>
 
-                <div class={totalProfit < 0 ? "total-loss" : "total-profit"}>
-                    {$number(totalProfit.toFixed(2), {
-                        locale: "en",
-                    })} €
+                    <div
+                        class={totalProfit < 0 ? "total-loss" : "total-profit"}
+                    >
+                        {$number(totalProfit, {
+                            locale: numberLocale,
+                            style: "currency",
+                            currency: "EUR",
+                        })}
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
 
-    <Table hover striped borderless size="sm mt-4">
-        <thead>
-            <td class="first">Name</td>
-            <td>Shares</td>
-            <td>Symbol</td>
-            <td>Purchase Price</td>
-            <td>Quote</td>
-            <td>Market Value</td>
-            <td style="text-align: right;">Profit / Loss</td>
-            <td>Actions</td>
-        </thead>
-        <tbody>
-            {#each entries as entry, index}
-                <tr>
-                    <td width="500">{entry.investment.name}</td>
-                    <td>{entry.shares}</td>
-                    <td>{entry.investment.ticker}</td>
-                    <td
-                        >{(
-                            entry.record.fifoCostPerSharesHeld.amount /
-                            100000000.0
-                        ).toFixed(2)} €</td
-                    >
-                    <td>{(entry.price.value / 100000000.0).toFixed(2)} €</td>
-                    <td
-                        >{(
-                            (entry.price.value / 100000000.0) *
-                            entry.shares
-                        ).toFixed(2)} €</td
-                    >
-                    <td style="text-align: right;">
-                        <div class={profitOrLoss(entry)}>
-                            {(
-                                entry.record.capitalGainsOnHoldings.amount /
-                                100.0
-                            ).toFixed(2)} €
-                        </div>
-                    </td>
-                </tr>
-            {/each}
-        </tbody>
-    </Table>
-</div>
+        <Table hover striped borderless size="sm mt-4">
+            <thead>
+                <td class="first">Name</td>
+                <td>Shares</td>
+                <td>Symbol</td>
+                <td>Purchase Price</td>
+                <td>Quote</td>
+                <td>Market Value</td>
+                <td style="text-align: right;">Profit / Loss</td>
+                <td>Actions</td>
+            </thead>
+            <tbody>
+                {#each snapshot.positions as position, index}
+                    <tr>
+                        <td width="500">{position.investment.name}</td>
+                        <td>{position.shares}</td>
+                        <td>{position.investment.ticker}</td>
+                        <td
+                            >{(
+                                position.record.fifoCostPerSharesHeld.amount /
+                                100000000.0
+                            ).toFixed(2)} €</td
+                        >
+                        <td>
+                            {$number(position.price.value / 100000000.0, {
+                                locale: numberLocale,
+                                style: "currency",
+                                currency: currency,
+                            })}
+                        </td>
+                        <td
+                            >{$number(
+                                (position.price.value / 100000000.0) *
+                                    position.shares,
+                                {
+                                    locale: numberLocale,
+                                    style: "currency",
+                                    currency: currency,
+                                }
+                            )}</td
+                        >
+                        <td style="text-align: right;">
+                            <div
+                                class={position.record.capitalGainsOnHoldings
+                                    .amount < 0
+                                    ? "loss"
+                                    : "profit"}
+                            >
+                                {$number(
+                                    position.record.capitalGainsOnHoldings
+                                        .amount / 100.0,
+                                    {
+                                        locale: numberLocale,
+                                        style: "currency",
+                                        currency: currency,
+                                    }
+                                )}
+                            </div>
+                        </td>
+                    </tr>
+                {/each}
+            </tbody>
+        </Table>
+    </div>
+{/if}
 
 <style>
     #details {
